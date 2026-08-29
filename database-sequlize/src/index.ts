f317c9b118113ad80c12f678a14b13e0002f74de
HEDI <admin@otamoon.hu>
2026-08-29 18:00:44 +0200

    added Model exports

--- database-sequlize/src/index.ts
+++ database-sequlize/src/index.ts
@@ -1,2 +1,3 @@
 export { Connect, DatabaseDialect } from './connect/connect';
 export { findOne, findAll, findByPk, exists, create, update, destroy, count, transaction } from './wrapper/databaseWrap';
+export { Model, DataTypes, Sequelize } from 'sequelize';

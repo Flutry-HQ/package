36175b79fdbc7f615cf606b6ef0cf8d7451f5c13
HEDI <admin@otamoon.hu>
2026-08-30 18:40:46 +0200

    feat(database-sequlize) added OP veriable

--- database-sequlize/src/index.ts
+++ database-sequlize/src/index.ts
@@ -1,3 +1,3 @@
 export { Connect, DatabaseDialect } from './connect/connect';
 export { findOne, findAll, findByPk, exists, create, update, destroy, count, transaction } from './wrapper/databaseWrap';
-export { Model, DataTypes, Sequelize } from 'sequelize';
+export { Model, DataTypes, Sequelize, Op } from 'sequelize';
